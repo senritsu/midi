b8d3050c8d9e275c794fad2b730330e57987e9cd
senritsu <teh.b4k4@gmail.com>
2021-05-01 11:24:58 +0200

    :heavy_plus_sign: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
   extends: [
-    // add more generic rulesets here, such as:
-    // 'eslint:recommended',
     'plugin:vue/vue3-recommended',
-    // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+    'eslint:recommended',
+    'prettier',
+    'prettier/vue',
+    // '@vue/typescript',
   ],
-  rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
-  }
+  plugins: ['vue'],
 }
