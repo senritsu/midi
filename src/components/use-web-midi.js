593805bbae21359db6b4d0459ac03b78d799153c
senritsu <teh.b4k4@gmail.com>
2021-05-01 14:23:02 +0200

    :sparkles: add webmidi composition function

---
+++ src/components/use-web-midi.js
@@ -0,0 +1,53 @@
+import { ref, onUnmounted } from 'vue'
+
+export default function useWebMidi () {
+  const state = ref('pending')
+  const inputs = ref({})
+  const outputs = ref({})
+
+  onUnmounted(() => {
+    Object.values(inputs.value).forEach((port) => {
+      port.close()
+    })
+    Object.values(outputs.value).forEach((port) => {
+      port.close()
+    })
+  })
+
+  const connect = async () => {
+    try {
+      const midiAccess = await navigator.requestMIDIAccess({
+        sysex: false,
+      })
+
+      inputs.value = Object.fromEntries(midiAccess.inputs)
+      outputs.value = Object.fromEntries(midiAccess.outputs)
+
+      midiAccess.onstatechange = ({ port }) => {
+        const lookup = port.type === 'input' ? inputs.value : outputs.value
+
+        if (port.state === 'disconnected' && port.id in lookup) {
+          delete lookup[port.id]
+        }
+        if (port.state === 'connected' && !(port.id in lookup)) {
+          lookup[port.id] = port
+        }
+      }
+
+      console.log(inputs.value)
+      state.value = 'connected'
+    } catch (error) {
+      console.error(`midi access failed: ${error}`)
+
+      state.value = 'error'
+    }
+  }
+
+  connect()
+
+  return {
+    state,
+    inputs,
+    outputs
+  }
+}
