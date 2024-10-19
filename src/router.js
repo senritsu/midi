f978df7c3aefd3c0ed4fe4739eb88b5cc9dbbe63
senritsu <jonas.schiegl@gmail.com>
2024-10-19 09:04:54 +0200

    make components dynamic

--- src/router.js
+++ src/router.js
@@ -1,15 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Home from './components/Home.vue'
-import GamepadMidiMapping from './components/GamepadMidiMapping.vue'
-import ChordsExperiment from './components/ChordsExperiment.vue'
-
 export default createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: Home },
-    { path: '/gamepad-midi', component: GamepadMidiMapping },
-    { path: '/chords', component: ChordsExperiment },
-    { path: '/redirect', component: () => import('./components/MidiRedirect.vue') },
-  ]
+    { path: '/', component: () => import('./components/Home.vue') },
+    {
+      path: '/gamepad-midi',
+      component: () => import('./components/GamepadMidiMapping.vue'),
+    },
+    {
+      path: '/chords',
+      component: () => import('./components/ChordsExperiment.vue'),
+    },
+    {
+      path: '/redirect',
+      component: () => import('./components/MidiRedirect.vue'),
+    },
+  ],
 })
